c6c42274aab366fff127e3ab99447b30def692be
Dan K <itsdanya@gmail.com>
2016-03-05 02:00:53 +0300

    Whitelist trainer params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,31 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    return 'application' unless devise_controller?
+
+    if resource_name == :trainer
+      'trainer'
+    elsif resource_name == :admin
+      'admin'
+    else
+      'application'
+    end
+  end
+
+  def configure_permitted_parameters
+    if resource_name == :trainer
+      devise_parameter_sanitizer.permit(:sign_up, keys: [
+        :first_name, :last_name, :phone_number, :description, :place_of_work,
+        :specialization_id, :avatar, :banner, :category, :experience
+      ])
+    end
+  end
 end
