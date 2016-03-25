ead087f1128ea86b03949a8f30a9eac48348e5a3
Dan K <itsdanya@gmail.com>
2016-03-25 20:46:15 +0300

    Annotate fixtures and tests

--- test/models/stage_test.rb
+++ test/models/stage_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: stages
+#
+#  id         :integer          not null, primary key
+#  position   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  program_id :integer
+#
+
 require 'test_helper'
 
 class StageTest < ActiveSupport::TestCase
