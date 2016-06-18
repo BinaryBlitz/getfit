4aa81b8c506809bc9b5e5c3edfcfa79eeac1e006
Dan K <itsdanya@gmail.com>
2016-06-18 18:56:43 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 
 specialization = Specialization.create!(name: 'Specialization')
 program_type = ProgramType.create!(name: 'Program type')
-image = File.open('public/blank.jpg')
+image = File.open(Rails.root.join('public', 'blank.jpg'))
+base64_image = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=='
 
 trainer = Trainer.create!(
   email: 'baz@qux.com',
@@ -24,21 +25,58 @@ VerificationToken.create!(phone_number: '+79998887766', verified: true)
 user = User.create!(
   first_name: 'Foo', last_name: 'Bar', phone_number: '+79998887766',
   description: 'Description',
-  avatar: image, banner: image,
+  avatar: base64_image, banner: base64_image,
   height: 180, weight: 80,
   birthdate: 20.years.ago, gender: 'male'
 )
 user.update!(api_token: 'foobar')
 
+trainer.followings.create!(user: user)
+trainer.ratings.create!(user: user, content: 'Rating', value: 1)
+
+subscription = trainer.subscriptions.create!(user: user)
+subscription.messages.create!(
+  [
+    { content: 'Trainer', category: 'trainer' },
+    { content: 'User', category: 'user' }
+  ]
+)
+
 post = Post.create!(content: 'Content', image: image, trainer: trainer)
+
+post.comments.create!(content: 'Content', author: user)
+post.likes.create!(user: user)
 
 program = Program.create!(
   name: 'Program', preview: 'Preview', description: 'Description',
-  banner: image, duration: 100, price: 100,
-  trainer: trainer, program_type: program_type
+  banner: image, price: 100,
+  trainer: trainer, program_type: program_type,
+  approved: true
 )
 
+program.purchases.create!(user: user)
+program.ratings.create!(user: user, content: 'Rating', value: 1)
+
+workout = program.workouts.create!(name: 'Workout', duration: 1)
+
+exercise_type = ExerciseType.create!(
+  name: 'Exercise type',
+  description: 'Description',
+  trainer: trainer
+)
+
+workout.exercises.create!(
+  [
+    { exercise_type: exercise_type, sets: 1, reps: 1 },
+    { exercise_type: exercise_type, weight: 1, distance: 1 }
+  ]
+)
+
+workout.workout_sessions.create!(user: user, scheduled_for: 1.month.from_now)
+
 admin = Admin.create!(email: 'foo@bar.com', password: 'qwerty123')
+
+Notification.create!(content: 'Notification')
 
 # Push notifications
 
