fa85cd88b11bf819d13507c794a03d743e414d4b
Dan K <itsdanya@gmail.com>
2016-08-11 18:06:51 +0900

    Add default URL options to ActionMailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # ActionMailer
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.smtp_domain }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: Rails.application.secrets.smtp_address,
