66d38fc02b560202adb7b5fbe9bd35aac90e3d85
SumLare <mrhard95@gmail.com>
2016-10-14 14:38:43 +0300

    Create conversation

---
+++ app/models/conversation.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: conversations
+#
+#  id         :integer          not null, primary key
+#  trainer_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Conversation < ApplicationRecord
+  belongs_to :trainer
+
+  has_many :tickets, dependent: :destroy
+end
