7fc2b06ed87f883773934c3b4c90846ebbe6a942
Dan Kim <itsdanya@gmail.com>
2017-03-08 21:20:39 +0300

    Update Notifier

--- app/services/notifier.rb
+++ app/services/notifier.rb
@@ -14,26 +14,40 @@ class Notifier
   def push
     return if @message.blank? || @device_token.blank?
 
-    Rails.logger.debug "#{Time.zone.now} Notifying #{@user} with message: #{@message}"
+    Rails.logger.debug "#{Time.zone.now} Notifying #{@user.id} with message: #{@message}"
 
     case @platform
-    when 'android'
-      push_android_notification
-    when 'ios'
-      push_ios_notification
+    when 'ios' then push_ios_notification
+    when 'android' then push_android_notification
     end
   end
 
   private
 
-  def push_android_notification
-    return if @message.blank? || @device_token.blank?
-    # Send GCM notification
-    Rails.logger.debug "#{Time.zone.now} GCM notification: #{@message}, user: #{@user.id}"
+  def push_ios_notification
+    notification = Houston::Notification.new(
+      token: @device_token,
+      alert: @message, badge: 1, custom_data: @options
+    )
+    client.push(notification)
+
+    Rails.logger.debug "#{Time.zone.now} iOS notification: #{@message}, user: #{@user.id}"
   end
 
-  def push_ios_notification
-    # Send APNS notification
-    Rails.logger.debug "#{Time.zone.now} iOS notification: #{@message}, user: #{@user.id}"
+  def push_android_notification
+    gcm = GCM.new(Rails.application.secrets.gcm_api_key)
+    registration_ids = [@device_token]
+    options = { notification: { title: 'Статус заказа изменён', body: @message }, data: @options }
+    gcm.send(registration_ids, options)
+
+    Rails.logger.debug "#{Time.zone.now} Android notification: #{@message}, user: #{@user.id}"
+  end
+
+  def client
+    if Rails.application.secrets.apn_environment == 'production'
+      Houston::Client.production
+    else
+      Houston::Client.development
+    end
   end
 end
